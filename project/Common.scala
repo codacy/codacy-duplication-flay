e468fa67987decbd25222b243f714817fc0769f9
DMarinhoCodacy <david.marinho@codacy.com>
2025-08-01 16:27:36 +0100

    fix docker build

--- project/Common.scala
+++ project/Common.scala
@@ -25,7 +25,7 @@ object Common {
           Cmd("RUN", "mv /opt/codacy/docs /docs"),
           Cmd(
             "RUN",
-            s"""|apk add --no-cache bash ruby ruby-irb ruby-rake ruby-io-console ruby-bigdecimal ruby-json ruby-bundler 
+            s"""|apk add --no-cache bash ruby ruby-rake ruby-bundler 
                 |    libstdc++ tzdata bash ca-certificates build-base ruby-dev
                 |&& echo 'gem: --no-document' > /etc/gemrc
                 |&& gem install rake -v 13.3.0
